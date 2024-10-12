725b8ba8e6cc8b1cb86bcf74a79379f086d10fa4
ProfessionalSalmon <tterrakkee@gmail.com>
2024-10-12 19:10:48 +0700

    update_code

---
+++ Medium/Movie-Ratings/Short-solution.sql
@@ -0,0 +1,37 @@
+WITH t1 AS (
+    SELECT
+        TOP 1 name AS results
+    FROM
+        MovieRating m
+        JOIN users u ON m.user_id = u.user_id
+    GROUP BY
+        name
+    ORDER BY
+        COUNT(*) DESC,
+        name
+),
+t2 AS (
+    SELECT
+        TOP 1 title AS results
+    FROM
+        MovieRating m
+        JOIN Movies mo ON m.movie_id = mo.movie_id
+    WHERE
+        MONTH(created_at) = 2
+        AND YEAR(created_at) = 2020
+    GROUP BY
+        title
+    ORDER BY
+        AVG(rating * 1.0) DESC,
+        title
+)
+SELECT
+    *
+FROM
+    t1
+UNION
+ALL
+SELECT
+    *
+FROM
+    t2;
